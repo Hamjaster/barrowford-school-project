e5e2768ce4af02360ee04d7990a4eabc531de944
Abdullah Ahmad <a.ahmad@tapforce.com>
2025-08-11 22:48:20 +0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+const express = require("express");
+type Request = import("express").Request;
+type Response = import("express").Response;
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello from Express + TypeScript!");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
