a033e51f62cb8faed3bf4bc65d97b2f9918e1c41
Hamza <hamzasepal@gmail.com>
2025-10-29 13:08:57 +0500

    fixes : remove throwErr

--- src/config/index.ts
+++ src/config/index.ts
@@ -45,9 +45,3 @@ const requiredEnvVars = [
  
   'JWT_SECRET',
 ];
-
-const missingEnvVars = requiredEnvVars.filter(envVar => !process.env[envVar]);
-
-if (missingEnvVars.length > 0) {
-  throw new Error(`Missing required environment variables: ${missingEnvVars.join(', ')}`);
-}
